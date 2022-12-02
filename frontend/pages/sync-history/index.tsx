73899f5cfc16fd98542c934d34512b55e45c41da
Shawn Pereira <shawnpereira1999@gmail.com>
2022-12-03 01:55:10 +0530

    feat: Set up Sync Browser History Page

---
+++ frontend/pages/sync-history/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Container, Heading } from "@chakra-ui/react";
+import React from "react";
+
+export default function SyncHistoryPage() {
+  return (
+    <Container mt={4}>
+      <Heading>Sync Browser History Page</Heading>
+      
+    </Container>
+  );
+}
