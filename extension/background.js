90945c2533ecea1e4d74b7debe005613c72d3f23
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2022-12-03 12:42:08 +0530

    extension added

--- extension/background.js
+++ extension/background.js
@@ -1,19 +1,27 @@
 try{
-
+	function getHistoryFromChrome(history){
+		console.log("history", history);
+		const injectElement = document.createElement('div');
+		history.forEach((item) => {
+			injectElement.innerHTML += item.title + " <br/>"; 
+		});
+		document.body.appendChild(injectElement);
+	}
 	//ON page change
 	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	  if(changeInfo.status == 'complete'){
-		alert("Alert");
-	  //if (changeInfo.url) {
-		chrome.scripting.executeScript({
-		  files: ['contentScript.js'],
-		  target: {tabId: tab.id}
-		});
-	  //}
+		chrome.history.search({
+			'text': '',
+			'maxResults': 75
+	 	}, function(historyItem) {
+			chrome.scripting.executeScript({
+				function: getHistoryFromChrome,
+				target: {tabId: tab.id},
+				args:  [history=historyItem]
+			  });
+	 	});		
 	  }
 	});
-	
-  
   
   }catch(e){
 	console.log(e);
